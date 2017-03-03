4085cf8d7af059b82ede89e56522950f43729130
kinses38 <shaun.kinsella38@mail.dcu.ie>
2017-03-03 12:37:00 +0000

    fix task toggbleable bug

--- code/agile-task-tracker/src/cljs/agile_task_tracker/draggable_tasks.cljs
+++ code/agile-task-tracker/src/cljs/agile_task_tracker/draggable_tasks.cljs
@@ -1,32 +1,28 @@
 (ns agile-task-tracker.draggable-tasks
   (:require [reagent.core :as r]))
 
-(defn make-tasks-draggable []
+(defn make-tasks-toggleable [task-id]
   (.. (js/$ ".portlet")
       (addClass "ui-widget ui-widget-content ui-helper-clearfix
               ui-corner-all")
       (find ".portlet-header")
-      (addClass. "ui-widget-header ui-corner-all")
-      (prepend "<span class='ui-icon ui-icon-plusthick
-              portlet-toggle'></span>"))
-
-  (.click (js/$ ".portlet-toggle")
+      (addClass. "ui-widget-header ui-corner-all"))
+  (.log js/console task-id)
+  (.click (js/$ (str "." task-id))
           (fn []
             (this-as this
               (let [icon (js/$ this)]
-                (.toggleClass icon "ui-icon-minusthick
-                            ui-icon-plusthick")
-                (.toggle (.find (.closest icon ".portlet") ".portlet-content"))))))
-
-  )
+                (.toggleClass icon "ui-icon-minusthick ui-icon-plusthick")
+                (.toggle (.find (.closest icon ".portlet") ".portlet-content")))))))
 
 (defn create-task-portlet
   [task-map]
-  (let [id (:task-id task-map)
+  (let [task-id (:task-id task-map)
         title (:task-title task-map)
         description (:description task-map)]
-    [:div.portlet {:id id}
-     [:div.portlet-header title]
+    [:div.portlet {:id task-id}
+     [:div.portlet-header title
+      [:span {:class (str "ui-icon ui-icon-plusthick portlet-toggle "task-id)}]]
      [:div.portlet-content
       [:div {:class "progress"}
        [:div {:class "progress-bar"
@@ -38,7 +34,3 @@
 
 
 
-
-
-(defn create-progress-bar [estimate progress]
-  ())
